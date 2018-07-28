a5399d928c32e2f490fc9ffe7b96b186390b72c1
Alex Kiecker <kiecker223@gmail.com>
2018-07-27 23:17:24 -0500

    Got playermovementcomponent to a workable state, however I do need to figure out wtf is going on with its collider

--- Assets/Scripts/PlayerMovementComponent.cs
+++ Assets/Scripts/PlayerMovementComponent.cs
@@ -2,18 +2,122 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ControllerAxis
+{
+	LS_Horizontal,
+	LS_Vertical,
+	RS_Horizontal,
+	RS_Vertical,
+	L_Trigger,
+	R_Trigger,
+	NUM
+}
+
+public enum ControllerButtons
+{
+	Button_A,
+	Button_B,
+	Button_X,
+	Button_Y,
+	Bumper_L,
+	Bumper_R,
+	Button_Start,
+	LeftStickDown,
+	RightStickDown,
+	NUM
+}
+
+public static class ControllerMappings
+{
+	public static string[] Axises = new string[]
+	{
+		"JHorizontal",
+		"JVertical",
+		"rx axis",
+		"ry axis",
+		"9",
+		"10"
+	};
+
+	public static string[] Buttons = new string[]
+	{
+		"joystick button 0",
+		"joystick button 1",
+		"joystick button 2",
+		"joystick button 3",
+		"joystick button 4",
+		"joystick button 5",
+		"joystick button 7",
+		"joystick button 8",
+		"joystick button 9"
+	};
+
+	public static float GetAxis(ControllerAxis axis)
+	{
+		return Input.GetAxis(Axises[(int)axis]);
+	}
+
+	public static bool GetButton(ControllerButtons button)
+	{
+		return Input.GetButton(Buttons[(int)button]);
+	}
+
+	public static bool GetButtonDown(ControllerButtons button)
+	{
+		return Input.GetButtonDown(Buttons[(int)button]);
+	}
+
+	public static bool GetButtonUp(ControllerButtons button)
+	{
+		return Input.GetButtonUp(Buttons[(int)button]);
+	}
+
+	public static Vector2 GetRightStickDirection()
+	{
+		return new Vector2(GetAxis(ControllerAxis.RS_Horizontal), GetAxis(ControllerAxis.RS_Vertical));
+	}
+
+	public static Vector2 GetLeftStickDirection()
+	{
+		return new Vector2(GetAxis(ControllerAxis.LS_Horizontal), GetAxis(ControllerAxis.LS_Vertical));
+	}
+}
+
 public class PlayerMovementComponent : MonoBehaviour
 {
 	public float speed;
 	public Vector3 direction;
-	
+	public Vector3 velocity;
+	public Vector3 rbVel;
+	public GameObject playerObj;
+	private Rigidbody m_RigidBody;
 
 	void Start()
     {
+		m_RigidBody = playerObj.GetComponent<Rigidbody>();
 	}
 	
 	void Update()
     {
-		
+		Vector2 controllerDir = ControllerMappings.GetRightStickDirection();
+		if (controllerDir.magnitude > 1e-1f)
+		{
+			direction = new Vector3(controllerDir.x, 0f, -controllerDir.y).normalized;
+			playerObj.transform.forward = direction;
+		}
+		Vector2 movementPow = ControllerMappings.GetLeftStickDirection();
+		if (movementPow.magnitude > 1e-1f)
+		{
+			velocity = new Vector3(movementPow.x, 0f, -movementPow.y);
+			transform.Translate(velocity * 0.3f);
+		}
+		else
+		{
+			velocity = new Vector3(0f, 0f, 0f);
+		}
+		Vector3 oldPosition = transform.position;
+		oldPosition.x = playerObj.transform.position.x;
+		oldPosition.z = playerObj.transform.position.z;
+		transform.position = oldPosition;
 	}
 }
