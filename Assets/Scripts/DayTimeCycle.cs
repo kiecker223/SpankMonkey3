9050d93266f32e07c7d59c7d365b2967f8dc2081
Alex Kiecker <kiecker223@gmail.com>
2018-07-28 13:03:51 -0500

    Added daylight stuffs

---
+++ Assets/Scripts/DayTimeCycle.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct GameTime
+{
+	public float CurrentTime;
+	
+	public bool IsDayTime
+	{
+		get
+		{
+			return CurrentTime <= 12;
+		}
+	}
+
+	public bool IsNightTime
+	{
+		get
+		{
+			return CurrentTime > 12;
+		}
+	}
+}
+
+public static class DayTimeCycle
+{
+	private static GameTime time = new GameTime();
+	public static List<System.Action> SwitchToDaytimeActions = new List<System.Action>();
+	public static List<System.Action<float>> ToDaytimeTransitions = new List<System.Action<float>>();
+	public static List<System.Action> SwitchToNightTimeActions = new List<System.Action>();
+	public static List<System.Action<float>> ToNightTimeTransitions = new List<System.Action<float>>();
+	public static GameTime GetTime()
+	{
+		return time;
+	}
+
+	public static void Initialize()
+	{
+		if (SwitchToDaytimeActions.Count > 0)
+		{
+			foreach (var action in SwitchToDaytimeActions)
+			{
+				action();
+			}
+		}
+		time.CurrentTime = 0f;
+	}
+
+	static void CallDayTimeTransitions(float f)
+	{
+		if (ToDaytimeTransitions.Count > 0)
+		{
+			foreach (var action in ToDaytimeTransitions)
+			{
+				action(f);
+			}
+		}
+	}
+
+	static void CallNightTimeTransitions(float f)
+	{
+		if (ToNightTimeTransitions.Count > 0)
+		{
+			foreach (var action in ToNightTimeTransitions)
+			{
+				action(f);
+			}
+		}
+	}
+
+	static float secondsToFullTransition = 4f;
+
+	public static void Tick(float dt)
+	{
+		if (Mathf.Floor(time.CurrentTime) == 12f)
+		{
+			if (secondsToFullTransition > 0f)
+			{
+				secondsToFullTransition -= dt;
+				CallNightTimeTransitions(secondsToFullTransition / 4f);
+				return;
+			}
+			if (SwitchToNightTimeActions.Count > 0)
+			{
+				foreach (var action in SwitchToNightTimeActions)
+				{
+					action();
+				}
+			}
+			// force to skip
+			time.CurrentTime = 13f;
+			secondsToFullTransition = 4f;
+		}
+		if (time.CurrentTime > 24f - 1e-1f)
+		{
+			if (secondsToFullTransition > 0f)
+			{
+				secondsToFullTransition -= dt;
+				CallDayTimeTransitions(secondsToFullTransition / 4f);
+				return;
+			}
+			if (SwitchToDaytimeActions.Count > 0)
+			{
+				foreach (var action in SwitchToDaytimeActions)
+				{
+					action();
+				}
+			}
+			time.CurrentTime = 0f;
+			secondsToFullTransition = 4f;
+		}
+		time.CurrentTime += dt;
+	}
+
+
+}
---
+++ Assets/Scripts/DaytimeLightController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaytimeLightController : MonoBehaviour
+{
+	private Light m_SunLight;
+	private Light m_MoonLight;
+	private Light m_ActiveLight;
+	public GameObject Sun;
+	public GameObject Moon;
+	private GameObject m_ActiveObject;
+
+	private float m_StartIntensity;
+	private float m_EndIntensity;
+
+	private Quaternion m_StartRotation;
+	private Quaternion m_EndRotation;
+
+	public Color dayTimeStartColor;
+	public Color dayTimeEndColor;
+
+	public Color nightTimeStartColor;
+	public Color nightTimeEndColor;
+
+	private Color m_LastColor;
+	private Color m_CurrentStartCol;
+	private Color m_CurrentEndCol;
+
+	private bool m_bDayTimeCycleInitialized = false;
+
+	void Awake()
+	{
+		DayTimeCycle.SwitchToDaytimeActions.Add(DayLightCycle);
+		DayTimeCycle.SwitchToNightTimeActions.Add(NightLightCycle);
+		DayTimeCycle.ToDaytimeTransitions.Add(TransitionToDays);
+		DayTimeCycle.ToNightTimeTransitions.Add(TransitionToNights);
+	}
+
+	void Start()
+	{
+		m_SunLight = Sun.GetComponent<Light>();
+		m_MoonLight = Moon.GetComponent<Light>();
+		m_StartRotation = Quaternion.Euler(0f, -30f, 0f);
+		m_EndRotation = Quaternion.Euler(179.99f, -30f, 0f);
+		Debug.Log("Thje thing work");
+		Moon.SetActive(false);
+	}
+
+	private float DayTimeRatio()
+	{
+		float Result = 0f;
+		if (DayTimeCycle.GetTime().IsDayTime)
+		{
+			Result = DayTimeCycle.GetTime().CurrentTime / 12;
+		}
+		else if (DayTimeCycle.GetTime().IsNightTime)
+		{
+			Result = (DayTimeCycle.GetTime().CurrentTime - 12) / 12;
+		}
+		return Result;
+	}
+
+	bool bNightFirst = true;
+	void TransitionToNights(float f)
+	{
+		float factor = Mathf.Clamp(1 - f, 0f, 1f);
+		if (bNightFirst)
+		{
+		}
+		m_ActiveLight.intensity = Mathf.Lerp(m_EndIntensity, 0f, factor);
+	}
+
+	bool bDayFirst = true;
+	void TransitionToDays(float f)
+	{
+		float factor = Mathf.Clamp(1 - f, 0f, 1f);
+		if (bDayFirst)
+		{
+		}
+		m_ActiveLight.intensity = Mathf.Lerp(m_EndIntensity, 0f, factor);
+	}
+
+	void NightLightCycle()
+	{
+		bNightFirst = true;
+		Moon.SetActive(true);
+		Sun.SetActive(false);
+		m_ActiveLight = m_MoonLight;
+		m_StartIntensity = 0.6f;
+		m_EndIntensity = 0.6f;
+		m_ActiveObject = Moon;
+		m_CurrentStartCol = nightTimeStartColor;
+		m_CurrentEndCol = nightTimeEndColor;
+	}
+
+	void DayLightCycle()
+	{
+		bDayFirst = true;
+		Moon.SetActive(false);
+		Sun.SetActive(true);
+		m_ActiveLight = m_SunLight;
+		m_StartIntensity = 1.0f;
+		m_EndIntensity = 0.8f;
+		m_ActiveObject = Sun;
+		m_CurrentStartCol = dayTimeStartColor;
+		m_CurrentEndCol = dayTimeEndColor;
+	}
+
+	void Update()
+	{
+		if (!m_bDayTimeCycleInitialized)
+		{
+			DayTimeCycle.Initialize();
+			m_bDayTimeCycleInitialized = true;
+		}
+		float dtRatio = DayTimeRatio();
+		m_ActiveObject.transform.rotation = Quaternion.Lerp(m_StartRotation, m_EndRotation, dtRatio);
+		m_ActiveLight.intensity = Mathf.Lerp(m_StartIntensity, m_EndIntensity, dtRatio);
+		m_ActiveLight.color = Color.Lerp(m_CurrentStartCol, m_CurrentEndCol, dtRatio);
+		DayTimeCycle.Tick(Time.deltaTime);
+	}
+}
