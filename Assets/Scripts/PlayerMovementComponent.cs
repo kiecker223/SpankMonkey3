8ef7c2ce751bfae3d7f1805fb61cd295fb388581
Alex Kiecker <kiecker223@gmail.com>
2018-07-27 20:52:08 -0500

    Added new folders and some assets

---
+++ Assets/Scripts/PlayerMovementComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementComponent : MonoBehaviour
+{
+	public float speed;
+	public Vector3 direction;
+	
+
+	void Start()
+    {
+	}
+	
+	void Update()
+    {
+		
+	}
+}
