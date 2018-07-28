cc736b8597165ac7b39f15a37fa4a18b68b4ffa6
Brian M. Foster <bfoster6@butlercc.edu>
2018-07-27 21:08:15 -0500

    setting up enemy controller

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+	Transform player;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("TestPlayerTemplate").transform;
+		agent = this.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.destination = player.position;
+	}
+}
