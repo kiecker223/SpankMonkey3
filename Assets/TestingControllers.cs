d2da6afa5e12afb0baa9b8525e0455282cbc5d3f
Brian M. Foster <bfoster6@butlercc.edu>
2018-07-28 12:50:25 -0500

    installed rewired and changed to BF PlayerController

---
+++ Assets/TestingControllers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingControllers : MonoBehaviour {
+
+	float p1RVert, p1RHori, p1LVert, p1LHori;
+	float p2RVert, p2RHori, p2LVert, p2LHori;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKey) print(p1LVert);
+	}
+}
