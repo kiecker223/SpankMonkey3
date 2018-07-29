0d2d8384243c8ce25c1a34dc7d5b03619c11cebc
Alex Kiecker <kiecker223@gmail.com>
2018-07-29 12:00:02 -0500

    final push

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,8 +6,9 @@ public class EnemySpawner : MonoBehaviour {
 
 	public GameObject enemy;
 	public int startingEnemies = 5;
+	public int enemiesSpawned = 0;
 	public static int totalEnemies = 0;
-	public static float MAX_ENEMIES = 20;
+	public static float MAX_ENEMIES = 100;
 	public static List<EnemySpawner> ActiveSpawners = new List<EnemySpawner>();
 	
 	public float interval = 10;
@@ -17,12 +18,11 @@ public class EnemySpawner : MonoBehaviour {
 	void Start() {
 		ActiveSpawners.Add(this);
 		startingEnemies = Random.Range(2,7);
-		MAX_ENEMIES = Random.Range(15,40);
 		health = Random.Range(8,12);
 	}
 	
 	public void SpawnEnemies() {
-		for(int i = 0; i < Random.Range(startingEnemies / 2, startingEnemies); i++) {
+		for(int i = 0; i < Mathf.FloorToInt(Random.value * startingEnemies / 2) + startingEnemies / 2; i++) {
 			if(totalEnemies < MAX_ENEMIES) {
 				Debug.Log("Spawning enemy");
 				Instantiate(enemy, transform.position, transform.rotation);
