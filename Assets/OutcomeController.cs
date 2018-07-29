aee944af61645a87ca5f7ced79cace1b79db08c3
Brian M. Foster <bfoster6@butlercc.edu>
2018-07-29 11:59:50 -0500

    making changes

--- Assets/OutcomeController.cs
+++ Assets/OutcomeController.cs
@@ -2,23 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Rewired;
 
 public class OutcomeController : MonoBehaviour {
 
-	public bool win = false;
+	Player player;
+	public int playerId = 0;
+
+	public bool win = false, start;
 	public TextMeshProUGUI text;
 
 	// Use this for initialization
 	void Start () {
+		player = ReInput.players.GetPlayer(playerId);
+
 		if(PlayerPrefs.GetInt("Win") == 0) {
 			text.text = "You have been touched!\nFinal score = " + PlayerPrefs.GetFloat("Score");
 		} else {
 			text.text = "You have escaped! Final score = " + PlayerPrefs.GetFloat("Score");
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update() {
+		start = player.GetButtonDown("Start");
+		if(start) Application.LoadLevel(0);
 	}
 }
