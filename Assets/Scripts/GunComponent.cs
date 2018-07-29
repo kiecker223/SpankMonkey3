191c5d1ac8dd66f7dad4f4f85915cc02d8f429b6
Brian M. Foster <bfoster6@butlercc.edu>
2018-07-29 09:54:12 -0500

    adding materials

--- Assets/Scripts/GunComponent.cs
+++ Assets/Scripts/GunComponent.cs
@@ -124,7 +124,7 @@ public class GunComponent : MonoBehaviour
 
 	private IEnumerator InternalReload()
 	{
-		yield return new WaitForSeconds(0.3f);
+		yield return new WaitForSeconds(1f);
 		ammo += currentClip;
 		if (ammo >= maxClip)
 		{
