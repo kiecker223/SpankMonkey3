a3f8b3b7946fc8f107aea41ecf4c18f57a19eb34
Brian M. Foster <bfoster6@butlercc.edu>
2018-07-29 11:48:48 -0500

    outcome screen

--- Assets/MainMenuControls.cs
+++ Assets/MainMenuControls.cs
@@ -8,10 +8,12 @@ public class MainMenuControls : MonoBehaviour {
 	public int playerId = 0;
 	Player player;
 	bool start, select;
+	public Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		player = ReInput.players.GetPlayer(playerId);
+		anim.SetBool("MainMenu", true);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/OutcomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class OutcomeController : MonoBehaviour {
+
+	public bool win = false;
+	public TextMeshProUGUI text;
+
+	// Use this for initialization
+	void Start () {
+		if(PlayerPrefs.GetInt("Win") == 0) {
+			text.text = "You have been touched!\nFinal score = " + PlayerPrefs.GetFloat("Score");
+		} else {
+			text.text = "You have escaped! Final score = " + PlayerPrefs.GetFloat("Score");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
